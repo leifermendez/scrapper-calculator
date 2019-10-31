f62000ba4d63703543ec323d344a8524781bd788
arturoluona <arturoluna879@gmail.com>
2019-10-31 01:31:46 -0400

    nuevo archivo para graficas

---
+++ example/calculator_grafica.php
@@ -0,0 +1,24 @@
+
+<?php
+
+include __DIR__ . "/../vendor/autoload.php";
+
+use leifermendez\scrapper_calculator\Grafica;
+
+$base_datos = array(
+    'server' => 'localhost',
+    'user' => 'root',
+    'pwd' => '',
+    'db' => 'idealista_csv'
+);
+$grafica = new Grafica($base_datos);
+$grafica->calculatorGrafica('global', '40.4238647', '-3.700173', 1, array('bano'=>7), 1500, 500);
+
+//function calculator(opciones: global || filters || precio, 'latitud', 'longitud', radio de busqueda en Km, array('filtro'=>valor), 'Precio Max', 'Precio Min')
+
+/**
+	Columnas en base de datos para especificar filtro de busqueda
+
+	 latitud, longitud, id,	titulo, anunciante,	descripcion, reformado,	telefonos, fecha,	tipo, precio, precioMetro, direccion, provincia, ciudad, calle, barrio,	distrito,	metrosCuadrados, bano, segundaMano,	armarioEmpotrado, construidoEn,	cocinaEquipada,	amueblado, cocinaEquipad, certificacionEnergetica, planta, exterior, interior, ascensor, aireAcondicionado,	habitaciones, balcon, trastero,	metrosCuadradosUtiles, piscina,	jardin,	parking, terraza, calefaccionIndividual, movilidadReducida,	mascotas
+**/
+
